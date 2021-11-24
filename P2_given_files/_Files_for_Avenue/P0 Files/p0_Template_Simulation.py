95e98c340383dc158e6b76fc04e0bb097292ea01
cfunn <57238795+cfunn@users.noreply.github.com>
2021-11-24 02:51:20 -0500

    Add files via upload

---
+++ P2_given_files/_Files_for_Avenue/P0 Files/p0_Template_Simulation.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('../')
+import time
+from Common_Libraries.p0_sim_lib import *
+
+import os
+from Common_Libraries.repeating_timer_lib import repeating_timer
+
+def update_sim ():
+    try:
+        my_qbot.ping()
+    except Exception as error_update_sim:
+        print (error_update_sim)
+
+
+speed = 0.1 # in m/s
+my_qbot = qbot(speed)
+
+#---------------------------------------------------------------------------------
+# STUDENT CODE BEGINS
+#---------------------------------------------------------------------------------
+
+
+
+
+
+
+#---------------------------------------------------------------------------------
+# STUDENT CODE ENDS
+#---------------------------------------------------------------------------------
+
+update_thread = repeating_timer(2, update_sim)
---
+++ P2_given_files/_Files_for_Avenue/P2 Files/p2_Template_Hardware.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('../')
+import time
+from Common_Libraries.p2_hardware_lib import *
+
+import os
+from Common_Libraries.repeating_timer_lib import repeating_timer
+
+arm = qarm()
+
+#---------------------------------------------------------------------------------
+# STUDENT CODE BEGINS
+#---------------------------------------------------------------------------------
+
+
+
+
+
+
+# Before turning off arm, run the following command in the shell.
+# arm.terminate_arm()
+
+#---------------------------------------------------------------------------------
+# STUDENT CODE ENDS
+#---------------------------------------------------------------------------------
